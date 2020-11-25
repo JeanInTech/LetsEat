e1c28fea12dc73cf8e67c1b4f7a7e90c1e5df8f4
Jean Thomas <jeniferjeanthomas@gmail.com>
2020-11-25 16:14:48 -0500

    Set up identity and secret class

---
+++ LetsEat/Models/UserFavoriteRecipes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LetsEat.Models
+{
+    public partial class UserFavoriteRecipes
+    {
+        public string UserId { get; set; }
+        public int RecipeId { get; set; }
+
+        public virtual FavoriteRecipes Recipe { get; set; }
+        public virtual AspNetUsers User { get; set; }
+    }
+}
